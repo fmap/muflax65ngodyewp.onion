e58c0665efd57c3d7d2fb2b90e43823ba7f6e332
muflax <mail@muflax.com>
2011-09-04 19:22:08 +0200

    * rss feed * changelog * basic sanity check for episteme headers * google analytics * episteme header

--- lib/default.rb
+++ lib/default.rb
@@ -34,12 +34,12 @@ def techne status
   end
 end
 
-def episteme status
+def episteme_cat status
   s = case status
       when :broken
         "partly believed"
       when :discredited
-        "not believed anymore"
+        "not believed"
       else
         status.to_s
       end
@@ -49,4 +49,52 @@ end
 # only articles that actually get printed
 def printed_items
   @items.select { |i| not i[:is_hidden] and not i.binary? }
+end
+
+# build rss feed
+def rss_feed
+  require "rss/maker"
+  version = "2.0"
+
+  # find changelog
+  log = @items.find{|i| i.identifier.match %r{/changelog/}}
+  
+  content = RSS::Maker.make(version) do |rss|
+    rss.channel.title = "Lies and Wonderland"
+    rss.channel.link = "http://muflax.com"
+    rss.channel.author = "mail@muflax.com"
+    rss.channel.description = "Lies and Wonderland"
+    rss.channel.date = log.mtime
+    rss.items.do_sort = true # sort items by date
+
+    changes(log).each do |change|
+      i = rss.items.new_item
+      i.title = "muflax hath written unto you..."
+      i.link = "http://muflax.com/changelog/"
+      i.date = Time.parse(change[:date])
+      i.description = change[:description]
+    end
+  end
+
+  content
+end
+
+# return changes based on changelog
+def changes log
+  require 'nokogiri'
+
+  changes = []
+
+  # parse log
+  html_log = Nokogiri::HTML(log.compiled_content)
+  html_log.css("ul#changelog").each do |ul|
+    ul.css("li").each do |li|
+      change = {}
+      change[:description] = li.children.to_s.strip
+      change[:date] = li.content[%r{\d{4}/\d{2}/\d{2}}]
+      changes << change
+    end
+  end
+
+  changes
 end
