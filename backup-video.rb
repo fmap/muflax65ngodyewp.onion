3b1c4948956df0a27788b22dd286d741f196d953
muflax <mail@muflax.com>
2011-11-12 05:24:48 +0100

    added basic backup script for videos

---
+++ backup-video.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+# Copyright muflax <mail@muflax.com>, 2011
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+hosts = [
+         "youtube.com/v",
+         "youtube.com/watch",
+         "vimeo.com",
+         "dailymotion.com",
+         "video.google.com",
+        ]
+
+urls = `grep -horP 'http://.*(#{hosts.join "|"})[^\"]*\\b' content/ | sort -u`
+
+Dir.chdir "video-backup"
+urls.split.each do |url|
+  puts "backing up #{url}..."
+  system "cclive -c --max-retries 0 '#{url}'"
+end
