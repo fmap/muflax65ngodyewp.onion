cfa1ae89c2e13edaef15c0b1380e8d815b17e013
muflax <mail@muflax.com>
2011-12-25 15:59:10 +0100

    backup links, too

---
+++ backup-links.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+# Copyright muflax <mail@muflax.com>, 2011
+# License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
+
+source ~/.zsh/path.sh
+
+grep -oP "http://.*" content/references.mkd | backup-urls
