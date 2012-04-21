db1c3efb53eb81795388fbed7436bc32b6d48b8a
muflax <mail@muflax.com>
2012-04-22 00:11:05 +0200

    create new log entry

---
+++ commands/create_log.rb
@@ -0,0 +1,17 @@
+usage       'create_log'
+summary     'create new log entry'
+description 'Opens new log entry in Emacs.'
+
+module Nanoc::CLI::Commands
+  class CreateLog < ::Nanoc::CLI::CommandRunner
+    def run
+      page = daily_logs.last.gsub(/(\d+)/) {|s| s.to_i + 1}
+      puts "editing: #{page}..."
+      system "emacs-gui #{page}"
+    end
+  end
+end
+
+runner Nanoc::CLI::Commands::CreateLog
+
+
