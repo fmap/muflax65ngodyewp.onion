f13c41b78df947b862370470fbcbb62fb8b80d85
muflax <mail@muflax.com>
2012-05-30 06:55:19 +0200

    fix with latest wc changes

--- commands/last.rb
+++ commands/last.rb
@@ -5,11 +5,19 @@ description 'Opens last page in Emacs.'
 module Nanoc::CLI::Commands
   class Last < ::Nanoc::CLI::CommandRunner
     def run
-      page = daily_logs.last
+      site = load_site "daily"
+
+      logs = site.items_by_date.select do |i|
+        i.reps.any? {|r| r.name == :wordcount}
+      end
+
+      page = logs.last[:filename]
       puts "editing: #{page}..."
       system "emacs-gui #{page}"
     end
   end
+
+  
 end
 
 runner Nanoc::CLI::Commands::Last
