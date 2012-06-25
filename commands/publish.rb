231063aecc90494da01d0e88012c6f1199d9989b
muflax <mail@muflax.com>
2012-06-25 13:23:01 +0200

    remove rss command for now

--- commands/publish.rb
+++ commands/publish.rb
@@ -23,9 +23,6 @@ module Nanoc::CLI::Commands
       # prepare images
       cmd "nanoc images"
 
-      # universal rss
-      cmd "nanoc rss"
-
       sites_arg(options[:sites]).each do |site|
         puts "publishing site: #{site}"
         
