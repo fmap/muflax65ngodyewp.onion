5bb3ea7cb6fa8369846e5798e1a4fff2f2e6c51d
muflax <mail@muflax.com>
2012-06-25 12:49:29 +0200

    finished log, fixed rss feeds, usable fancy links

--- lib/rss.rb
+++ lib/rss.rb
@@ -59,3 +59,30 @@ def changes log
 
   changes
 end
+
+def rss_feed_merged
+  require "rss"
+  version = "2.0"
+
+  date = Time.new 0
+  items = []
+
+  # read individual feeds and include them
+  @site.site_yaml["sites"].keys.each do |name|
+    feed = RSS::Parser.parse(File.open("out/#{name}/rss.xml"))
+    date = [feed.channel.date, date].max
+    items.concat feed.items
+  end
+
+  # build merged feed
+  merged_feed = RSS::Maker.make(version) do |rss|
+    rss.channel.title = "muflax"
+    rss.channel.link = "http://muflax.com"
+    rss.channel.author = "mail@muflax.com"
+    rss.channel.description = "read ALL the muflax!"
+  end
+  merged_feed.items.concat items.sort_by(&:date).reverse.take(10)
+
+  # write it
+  merged_feed
+end
