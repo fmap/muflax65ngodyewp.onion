76498e48fc0df92c2aab2ade0bb27e918361add6
muflax <mail@muflax.com>
2012-07-27 22:17:15 +0200

    tmux

--- session.sh
+++ session.sh
@@ -2,19 +2,20 @@
 # Copyright muflax <mail@muflax.com>, 2012
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
+site=$1
+
 if [[ -z $1 ]]; then
-  echo "usage: $0 site"
-  exit 1
+  echo "defaulting to daily..."
+  site="daily"
+fi
+
+if [[ $site -eq "daily" ]]; then
+  # work on last log
+  nanoc last
 fi
 
 # start a screen session and set up basic tools
-screen -dmS nanoc nanoc -s $1 view
-screen -S nanoc -X screen -t watch 1 nanoc -s 1 watch
-screen -S nanoc -X screen -t mc 2 zsh -c mc
-screen -S nanoc -X screen 3 zsh
-
-# basic emacs session
-emacs-gui Rules
-
-# switch to screen session
-screen -r nanoc
+tmux new-session "nanoc view -s $site" \; \
+  neww "nanoc watch -s $site" \; \
+  set default-path "$(pwd)" \; set -g -a update-environment ' PWD' \; \
+  neww
