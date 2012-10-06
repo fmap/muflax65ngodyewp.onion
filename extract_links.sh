beed938e06fa5bbfcc74cbef2225b18d85c5a4c2
muflax <mail@muflax.com>
2012-10-06 22:34:38 +0200

    minor link cleanup, don't backup own site

--- extract_links.sh
+++ extract_links.sh
@@ -2,5 +2,5 @@
 # Copyright muflax <mail@muflax.com>, 2012
 # License: GNU GPL 3 <http://www.gnu.org/copyleft/gpl.html>
 
-grep -ahorP '(https?[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]' content*/**/*.(mkd|org)
+grep -ahorP '(https?[.:][^ >"\t]*|www\.[-a-z0-9.]+)[^ .,;\t>">\):]' content*/**/*.(mkd|org) | grep -v "muflax\.com"
 
