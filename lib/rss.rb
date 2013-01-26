7f7b56fea66291357f1b8748df3efe7f09f10fc6
muflax <mail@muflax.com>
2013-01-26 09:10:33 +0100

    new log, major site cleanup

--- lib/rss.rb
+++ lib/rss.rb
@@ -3,9 +3,6 @@ def rss_feed
   require "rss/maker"
   version = "2.0"
 
-  # find changelog
-  log = @items.find{|i| i.identifier.match %r{/changelog/}}
-  
   content = RSS::Maker.make(version) do |rss|
     rss.channel.title = @site.title
     rss.channel.link = @site.url
@@ -14,22 +11,12 @@ def rss_feed
     rss.items.do_sort = true # sort items by date
 
     # use changelog or non-draft articles
-    if not log.nil?
-      changes(log).each do |change|
-        i = rss.items.new_item
-        i.title = "muflax hath written unto you..."
-        i.link = "#{@site.url}/changelog/"
-        i.date = Time.parse(change[:date])
-        i.description = change[:description]
-      end
-    else
-      @site.items_by_date.last(5).each do |item|
-        i = rss.items.new_item
-        i.title = "#{item[:title]}"
-        i.link = "#{@site.url}" + item.path
-        i.date = item[:date].to_time
-        i.description = tidy(item.compiled_content).force_encoding("utf-8")
-      end
+    @site.items_by_date.last(5).each do |item|
+      i = rss.items.new_item
+      i.title = "#{item[:title]}"
+      i.link = "#{@site.url}" + item.path
+      i.date = item[:date].to_time
+      i.description = tidy(item.compiled_content).force_encoding("utf-8")
     end
 
     # mod date is newest article / entry in log
@@ -38,26 +25,6 @@ def rss_feed
   end
 
   content
-end
-
-# return changes based on changelog
-def changes log
-  require 'nokogiri'
-
-  changes = []
-
-  # parse log
-  html_log = Nokogiri::HTML(log.compiled_content)
-  html_log.css("ul#changelog").each do |ul|
-    ul.css("li").each do |li|
-      change = {}
-      change[:description] = li.children.to_s.strip
-      change[:date] = li.content[%r{\d{4}/\d{2}/\d{2}}]
-      changes << change
-    end
-  end
-
-  changes
 end
 
 def rss_feed_merged
@@ -69,6 +36,8 @@ def rss_feed_merged
 
   # read individual feeds and include them
   @site.site_yaml["sites"].keys.each do |name|
+    next if name == "muflax"
+    
     feed = RSS::Parser.parse(File.open("out/#{name}/rss.xml"))
     date = [feed.channel.date, date].max
     feed.items.each do |item|
